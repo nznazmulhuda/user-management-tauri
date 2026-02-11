7be44de3a4ee4753f2514fdcff63da5f2bed368a
Nazmul Huda <nznazmulhuda04@gmail.com>
2026-02-12 02:30:14 +0600

    backend added

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::process::Command;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -8,6 +9,16 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|_app| {
+            #[cfg(not(debug_assertions))] // Only in production build
+            {
+                Command::new("backend") // backend.exe name without .exe
+                    .spawn()
+                    .expect("Failed to start backend");
+            }
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
